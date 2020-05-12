3df9e8fd3a1725382508efa2006e351b2f174cd6
Richard Butler <rpbutler@gmail.com>
2020-05-12 22:43:33 +0100

    Cleaned up JS

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.querySelector("#body");
-var randBtn = document.querySelector("#random");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.querySelector("#body");
+const randBtn = document.querySelector("#random");
 
+const setGradient = () => {
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  css.textContent = `background: linear-gradient(to right, ${color1.value}, ${color2.value})`;
+};
 
-
-function setGradient() {
-  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-
-  css.textContent = "background: linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-}
-
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
+const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
+};
 
-function setRandomColor() {
+const setRandomColor = () => {
   color1.value = getRandomColor();
   color2.value = getRandomColor();
   setGradient();
-}
+};
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
-
 
 randBtn.addEventListener("click", setRandomColor);
